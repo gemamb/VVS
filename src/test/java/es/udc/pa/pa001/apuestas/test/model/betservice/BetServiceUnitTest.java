0f623411c3180d126bf5f889afdc6c160ade23ad
raquel.lcacho <raquel.lcacho@udc.es>
2016-10-22 18:12:20 +0200

    Reestructuración de la ruta de ficheros de pruebas (Issue #6)

--- src/test/java/es/udc/pa/pa001/apuestas/test/model/betservice/BetServiceUnitTest.java
+++ src/test/java/es/udc/pa/pa001/apuestas/test/model/betservice/BetServiceUnitTest.java
@@ -1,4 +1,4 @@
-package es.udc.pa.pa001.apuestas.test.VVS;
+package es.udc.pa.pa001.apuestas.test.model.betservice;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
