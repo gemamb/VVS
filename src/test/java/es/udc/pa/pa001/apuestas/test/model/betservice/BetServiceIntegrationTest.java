589502e513809975874291efff2d515efc42c339
gema.marino.bodelon <gema.marino.bodelon@udc.es>
2016-10-27 16:13:13 +0200

    Creación clase de test de Integracion

---
+++ src/test/java/es/udc/pa/pa001/apuestas/test/model/betservice/BetServiceIntegrationTest.java
@@ -0,0 +1,16 @@
+package es.udc.pa.pa001.apuestas.test.model.betservice;
+
+import static es.udc.pa.pa001.apuestas.model.util.GlobalNames.SPRING_CONFIG_FILE;
+import static es.udc.pa.pa001.apuestas.test.util.GlobalNames.SPRING_CONFIG_TEST_FILE;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { SPRING_CONFIG_FILE, SPRING_CONFIG_TEST_FILE })
+@Transactional
+public class BetServiceIntegrationTest {
+
+}
